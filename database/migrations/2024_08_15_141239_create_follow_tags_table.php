bbeca7b17f3b0429ac63c0a14761176fe304db1b
kefayetur rahman <kefayeturrahman492@gmail.com>
2024-08-15 20:22:27 +0600

    follow_tag and FollowTag model and migration file have been created

---
+++ database/migrations/2024_08_15_141239_create_follow_tags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('follow_tags', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("is_follow")->default("false");
+            $table->foreignId("tag_id")->references("id")->on("tags")
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("user_id")->references("id")->on("users")
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('follow_tags');
+    }
+};
